1f40d8d2636980fe269373d2f0cf3d34582a8d8b
AyabulelaSityata <sityataayabulela@gmail.com>
2023-07-03 12:20:39 +0200

    first-commit

---
+++ src/components/FooterComp.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <p>Copyright &COPY; 2023</p>
+    </div>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/NavbarComp.vue
@@ -0,0 +1,44 @@
+<template>
+    <div>
+        <nav class="navbar navbar-expand-lg bg-body-tertiary">
+            <div class="container-fluid">
+              <router-link class="navbar-brand" to="/home">Navbar</router-link>
+              <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+              </button>
+              <div class="collapse navbar-collapse" id="navbarNavDropdown">
+                <ul class="navbar-nav">
+                  <li class="nav-item">
+                    <router-link class="nav-link active" aria-current="page" to="/home">Home</router-link>
+                  </li>
+                  <li class="nav-item">
+                    <router-link to="/about" class="nav-link" >About</router-link>
+                  </li>
+                  <li class="nav-item">
+                    <router-link class="nav-link" to="/resume">Resume</router-link>
+                  </li>
+                  <li class="nav-item">
+                    <router-link class="nav-link" to="/testimonials">Testimonials</router-link>
+                  </li>
+                  <li class="nav-item">
+                    <router-link class="nav-link" to="/projects">Projects</router-link>
+                  </li>
+                  <li class="nav-item">
+                    <router-link class="nav-link" to="contact">Contact</router-link>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </nav>
+    </div>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style scoped>
+
+</style>
